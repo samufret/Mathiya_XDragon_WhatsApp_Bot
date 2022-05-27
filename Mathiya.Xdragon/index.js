94bfb04cb464fd930a9ccb09e58672e6740b7a06
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-05-27 09:24:04 +0530

    Create index.js

---
+++ Mathiya.Xdragon/index.js
@@ -0,0 +1,26 @@
+/* Copyright (C) 2022 sohan-matheesha.
+
+Licensed under the  GPL-3.0 License;
+
+you may not use this file except in compliance with the License.
+
+https://github.com/Mathiya578/Mathiya_XDragon_WhatsApp_Bot
+
+*/
+
+module.exports = {
+
+    Base: require('./Base'),
+
+    Message: require('./Message'),
+
+    StringSession: require('./StringSession'),
+
+    ReplyMessage: require('./ReplyMessage'),
+
+    Image: require('./Image'),
+
+    Video: require('./Video')
+
+};
+
